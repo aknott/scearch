4370ca3bcf5c793d2015d02c30a59103a66d0835
Alex Knott <aknott@3vr.com>
2013-06-12 14:50:59 -0700

    Refactor main route to only use GET request instead of POST.  Set default sort to plays.

--- mainapp.py
+++ mainapp.py
@@ -4,18 +4,19 @@ from scsearchfast import Searcher
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST','GET'])
+@app.route('/', methods=['GET'])
 def mainapp():
-	if request.method == 'POST':
-		result=""
-		#result = str(request.form)
-		print request.form
-		input = request.form['searchstr']
-		sorttype = request.form['sort']
+	result=""
+	input = request.args.get('searchstr')
+	sorttype = request.args.get('sort')
+	if((input is not None and sorttype is not None) and input != ""):
 		print "searching " + input + " on " + sorttype
-		result = dosearch(input,sorttype)
-		return render_template('index.html', result=result)
-	return render_template('index.html', result="")
+		searcher = Searcher()
+		result = searcher.dosearch(input,sorttype)
+	if input is None:
+		input = ""
+	return render_template('index.html', input=input,result=result)
+
 
 @app.route('/search', methods=['POST','GET'])
 def search():
